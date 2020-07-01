68bc2d96af357f59030bb8d564eee0e80eae2353
João Lima <joaoplima@ua.pt>
2020-07-01 23:24:18 +0200

    Create a arrayList in the Player class. Methods to access the garage

---
+++ src/com/company/Player.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import com.company.StandVehicle.Car;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    // Parameters of the player
+    public String name;
+    public Double cash;
+
+    // Create an array list to store the cars from th player
+    private ArrayList<Car> garage =  new ArrayList<Car>();
+
+    public Player(String name, Double cash) {
+        this.name = name;
+        this.cash = cash;
+    }
+
+    // Add a car to the Garage
+    public void addCarToGarage(Car car){
+        garage.add(car);
+    }
+
+    // Print the list of cars
+    public void printGarageList(){
+        System.out.println("You have " + garage.size() + " cars in you garage");
+        for (int i = 0; i < garage.size(); i++) {
+            System.out.println((i+1) + ". " + garage.get(i));
+        }
+    }
+
+    // Remove Car from garage by the position
+    public void removeCarFromGarage(int position){
+        Car theCar = garage.get(position);
+        garage.remove(position);
+    }
+
+    // Check if the car is in the garage
+    public boolean isInGarage(Car car){
+        return garage.contains(car);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name  + " | Cash: " + cash ;
+    }
+}
