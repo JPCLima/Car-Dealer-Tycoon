36751d55109b9f3fa69db611e09562d3bdab3fec
João Lima <joaoplima@ua.pt>
2020-07-01 20:53:38 +0200

    Created class Player

---
+++ src/com/company/StandVehicle/Player.java
@@ -0,0 +1,12 @@
+package com.company.StandVehicle;
+
+public class Player {
+
+    public String name;
+    public Double cash;
+
+    public Player(String name, Double cash) {
+        this.name = name;
+        this.cash = cash;
+    }
+}
