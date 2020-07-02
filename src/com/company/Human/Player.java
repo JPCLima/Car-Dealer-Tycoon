c225fb396fdfbc404f4fdefe6da602a99d9ab4a3
João Lima <joaoplima@ua.pt>
2020-07-02 22:55:57 +0200

    Create a method sellVehicle() and buyVehicle()

--- src/com/company/Human/Player.java
+++ src/com/company/Human/Player.java
@@ -1,22 +1,23 @@
 package com.company.Human;
 
 import com.company.StandVehicle.Car;
+import com.company.StandVehicle.Vehicle;
 
 import java.util.ArrayList;
 
 public class Player extends Human {
 
     // Create an array list to store the cars from th player
-    private ArrayList<Car> garage =  new ArrayList<Car>();
+    private ArrayList<Vehicle> garage = new ArrayList<>();
 
     // Constructor of the Player using the super
-    public Player(String name, String cash) {
+    public Player(String name, Double cash) {
         super(name, cash);
     }
 
     // Add a car to the Garage
-    public void addCarToGarage(Car car){
-        garage.add(car);
+    public void addVehicleToGarage(Vehicle vehicle){
+        garage.add(vehicle);
     }
 
     // Print the list of cars
@@ -28,13 +29,43 @@ public class Player extends Human {
     }
 
     // Remove Car from garage by the position
-    public void removeCarFromGarage(Car car){
-        garage.remove(car);
+    public void removeVehicleFromGarage(Vehicle vehicle){
+        garage.remove(vehicle);
     }
 
     // Check if the car is in the garage
     public boolean isInGarage(Car car){
         return garage.contains(car);
+    }
+
+    // Method to buy a vehicle
+    public void buyVehicle(Vehicle vehicle, Double vehiclePrice){
+        if(this.cash >= vehiclePrice){
+            // Pay tax to clean the car
+            this.payTaxCleaning(vehiclePrice);
+            // The change money to the player
+            this.cash -= vehiclePrice;
+            // Add the car to the garage
+            this.addVehicleToGarage(vehicle);
+            System.out.println("Congratulations, you have a new vehicle in you garage");
+        }else{
+            System.out.println("You don't have enough money to buy this vehicle");
+        }
+    }
+
+    // Method to sell the car
+    public void sellVehicle(Vehicle vehicle, Double vehiclePrice){
+        // Pay tax to clean the car
+        this.payTaxCleaning(vehiclePrice);
+        // Add money to the cash of player
+        this.cash += vehiclePrice;
+        // Remove the car from the garage
+        this.removeVehicleFromGarage(vehicle);
+        System.out.println("Your vehicle has been sold");
+    }
+
+    private void payTaxCleaning(Double vehiclePrice) {
+        this.cash -= (vehiclePrice * 0.02);
     }
 
     @Override
