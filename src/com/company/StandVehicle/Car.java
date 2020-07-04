8eb69cfbd84a36c59ca6c8cf5ec8ea16043aac12
João Lima <joaoplima@ua.pt>
2020-07-04 12:50:16 +0200

    Changed name and data type of some of the parameters

--- src/com/company/StandVehicle/Car.java
+++
@@ -1,8 +0,0 @@
-package com.company.StandVehicle;
-
-public class Car extends Vehicle {
-
-    public Car(Double value, String brand, Double mileage, String colour, String segment, boolean brakesPerCent, boolean dampersPerCent, boolean enginePerCent, boolean carBodyPerCent, boolean gearBoxPerCent) {
-        super(value, brand, mileage, colour, segment, brakesPerCent, dampersPerCent, enginePerCent, carBodyPerCent, gearBoxPerCent);
-    }
-}
--- src/com/company/standOfVehicles/Vehicle.java
+++ src/com/company/standOfVehicles/Vehicle.java
@@ -1,6 +1,6 @@
-package com.company.StandVehicle;
+package com.company.standOfVehicles;
 
-public abstract class Vehicle {
+public abstract class Vehicle{
 
     // Parameters
     public Double value;
@@ -10,28 +10,28 @@ public abstract class Vehicle {
     public String segment;
 
     // Parts of car that can be repair
-    public Boolean brakesIsOk;
-    public Boolean dampersIsOk;
-    public Boolean engineIsOk;
-    public Boolean carBodyIsOk;
-    public Boolean gearBoxIsOk;
+    public String brakesCondition;
+    public String dampersCondition;
+    public String engineCondition;
+    public String carBodyCondition;
+    public String gearBoxCondition;
 
     // Define the state of the car: working or destroyed
     // The car can be destroyed if the PPHU Adrian will ruin the car
     public Boolean isWorking;
 
     // Constructor of vehicle
-    public Vehicle(Double value, String brand, Double mileage, String colour, String segment, Boolean brakesIsOk, Boolean dampersIsOk, Boolean engineIsOk, Boolean carBodyIsOk, Boolean gearBoxIsOk) {
+    public Vehicle(Double value, String brand, Double mileage, String colour, String segment, String brakesCondition, String dampersCondition, String engineCondition, String carBodyCondition, String gearBoxCondition) {
         this.value = value;
         this.brand = brand;
         this.mileage = mileage;
         this.colour = colour;
         this.segment = segment;
-        this.brakesIsOk = brakesIsOk;
-        this.dampersIsOk = dampersIsOk;
-        this.engineIsOk = engineIsOk;
-        this.carBodyIsOk = carBodyIsOk;
-        this.gearBoxIsOk = gearBoxIsOk;
+        this.brakesCondition = brakesCondition;
+        this.dampersCondition = dampersCondition;
+        this.engineCondition = engineCondition;
+        this.carBodyCondition = carBodyCondition;
+        this.gearBoxCondition = gearBoxCondition;
         // All car created are working
         this.isWorking = true;
     }
