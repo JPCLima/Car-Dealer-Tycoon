195406074eae219ad14be199f25718248c71c719
João Lima <joaoplima@ua.pt>
2020-07-07 20:55:56 +0200

    Initialize the list from DB in the Player class

--- src/com/company/human/Player.java
+++ src/com/company/human/Player.java
@@ -1,14 +1,24 @@
 package com.company.human;
 
+import com.company.DAO.ClientDAO;
+import com.company.DAO.VehicleDAO;
 import com.company.Transaction;
 import com.company.model.Client;
 import com.company.model.Vehicle;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-
 public class Player extends Human {
+
+    // Initialize ClientDAO and VehicleDAO
+    ClientDAO clientDAO = new ClientDAO();
+    VehicleDAO vehicleDAO = new VehicleDAO();
+
+    // Create a list to store the list of clients from DB
+    private List<Client> clientListDB;
+    private List<Vehicle> vehicleListDB;
 
     // Create a list to store the cars from th player
     private List<Vehicle> garage;
@@ -25,16 +35,14 @@ public class Player extends Human {
         garage = new ArrayList<>();
         potentialClientList = new ArrayList<>();
         transactions = new ArrayList<>();
+        clientListDB = clientDAO.getClientListDB();
+        vehicleListDB = vehicleDAO.getVehicleListDB();
     }
+
 
     // Add a vehicle to the Garage
     public void addVehicleToGarage(Vehicle vehicle){
         garage.add(vehicle);
-    }
-
-    // Add a vehicles to the Garage
-    public void  addVehicleToGarage(ArrayList<Vehicle> vehicleArrayList){
-        garage.addAll(vehicleArrayList);
     }
 
     // Remove Car from garage by the position
@@ -51,7 +59,7 @@ public class Player extends Human {
     public void printGarageList(){
         System.out.println("You have " + garage.size() + " cars in you garage");
         for (int i = 0; i < garage.size(); i++) {
-            System.out.println((i+1) + ". " + garage.get(i));
+            System.out.println("    " + (i+1) + ". " + garage.get(i));
         }
     }
 
@@ -94,22 +102,23 @@ public class Player extends Human {
 
     // Methods for the potential clients
     // Method to get client from DB using ID
-    private Client getClient(List<Client> clientsDB, Integer clientID){
-        if (clientsDB != null) {
-            return clientsDB.get(clientID);
+    private Client getClient(Integer clientID){
+        if (clientListDB != null) {
+            return clientListDB.get(clientID);
         }
         return null;
     }
 
     // Get random Client
     public Client getRandomClient(List<Client> clientsDB){
+        Client client = new Client();
         if(potentialClientList.size() != clientsDB.size()){
             // While will not find a different from the DB keep run
             boolean notAdded = true;
             while (notAdded){
                 // Generate a random number as ID
-                int randomNum = generateRandomNumber(0, clientsDB.size() - 1);
-                Client client =  getClient(clientsDB, randomNum);
+                int randomNum = generateRandomNumber(clientsDB.size() - 1);
+                client =  getClient(randomNum);
 
                 if(!potentialClientList.contains(client)){
                     potentialClientList.add(client);
@@ -117,27 +126,27 @@ public class Player extends Human {
                 }
             }
         }
-        return null;
+        return client;
     }
 
     // Get N Clients
-    public List<Client> getNRandomClients(List<Client> clientsDB, Integer numberOfClients){
-        // Create a new list to store the N initial clients
-        List<Client> initialClients = new ArrayList<>();
+    public void getNRandomClients(Integer numberOfClients){
+        // Make sure all the clients are not in the potentialClientList
+        if(potentialClientList.size() < clientListDB.size()){
+            // Create a new list to store the N initial clients
+            List<Client> initialClients = new ArrayList<>();
 
-        // Create a random number to choose the initial clients
-        // The clients cannot be repeated
-        while (initialClients.size() < numberOfClients){
-            Client client = getRandomClient(clientsDB);
+            // Create a random number to choose the initial clients
+            // The clients cannot be repeated
+            while (initialClients.size() < numberOfClients){
+                Client client = getRandomClient(clientListDB);
 
-            if(!potentialClientList.contains(client)){
-                initialClients.add(client);
+                if(potentialClientList.contains(client)){
+                    initialClients.add(client);
+                }
             }
         }
-
-        return initialClients;
     }
-
     // Print the list of cars
     public void printPotentialClientList(){
         System.out.println("You have " + potentialClientList.size() + " potential clients to make business");
@@ -146,26 +155,111 @@ public class Player extends Human {
         }
     }
 
-    public void getClientsMarketingCampaign (List<Client> clientsDB){
-        this.cash -= 250.0;
-        getNRandomClients(clientsDB, 5);
+    public void getClientsMarketingCampaign (){
+        int potentialClientsToAdd = clientListDB.size() - potentialClientList.size();
+        // If have full space add 5
+        if(potentialClientsToAdd == 5){
+            this.cash -= 250.0;
+            getNRandomClients( 5);
+        }else if(potentialClientsToAdd != 0){
+            this.cash -= 250.0;
+            getNRandomClients( potentialClientsToAdd);
+        }else{
+            System.out.println("You used all the potential clients");
+        }
     }
 
     // Methods to get more Potential Clients
-    public void getClientsAnnouncement (List<Client> clientsDB){
-        this.cash -= 150.0;
-        getNRandomClients(clientsDB, 3);
+    public void getClientsAnnouncement (){
+        int potentialClientsToAdd = clientListDB.size() - potentialClientList.size();
+        // If have full space add 3
+        if(potentialClientsToAdd == 3){
+            this.cash -= 150.0;
+            getNRandomClients(3);
+        }else if(potentialClientsToAdd != 0){
+            this.cash -= 150.0;
+            getNRandomClients( potentialClientsToAdd);
+        }else{
+            System.out.println("You used all the potential clients");
+        }
+
     }
 
     // Methods to get more Potential Clients
-    public void getClientsOnlineAnnouncement (List<Client> clientsDB){
-        this.cash -= 50.0;
-        getNRandomClients(clientsDB, 1);
+    public void getClientsOnlineAnnouncement (){
+        int potentialClientsToAdd = clientListDB.size() - potentialClientList.size();
+        // If have full space add 1
+        if(potentialClientsToAdd == 1){
+            this.cash -= 50.0;
+            getNRandomClients(1);
+        }else{
+            System.out.println("You used all the potential clients");
+        }
+    }
+
+    // Methods for the potential Vehicles
+    // Method to get vehicle from DB using ID
+    private Vehicle getVehicle(Integer vehicleID){
+        if (vehicleListDB != null) {
+            return vehicleListDB.get(vehicleID);
+        }
+        return null;
+    }
+
+    // Get a random Vehicle from DB
+    public void getRandomVehicle(){
+        Vehicle vehicle;
+        if(garage.size() != vehicleListDB.size()){
+            // While will not find a different from the DB keep run
+            boolean notAdded = true;
+            while (notAdded){
+                // Generate a random number as ID
+                int randomNum = generateRandomNumber(vehicleListDB.size() - 1);
+                vehicle =  getVehicle(randomNum);
+
+                if(!garage.contains(vehicle)){
+                    garage.add(vehicle);
+                    notAdded = false;
+                }
+            }
+        }
+    }
+
+    // Get available vehicles in the VehiclesDB
+    private List<Vehicle> getAvailableVehicles(){
+        List<Vehicle> availableVehiclesDB = new ArrayList<>();
+
+        if(garage.size() != vehicleListDB.size()){
+            // While will not find a different from the DB keep run
+            boolean notAdded = true;
+            while (vehicleListDB.size() - garage.size() != 0 ){
+                // Add just the vehicles which are not in the garage
+                for(Vehicle vehicle: vehicleListDB){
+                    if(!garage.contains(vehicle)){
+                        availableVehiclesDB.add(vehicle);
+                    }
+                }
+            }
+        }
+        return availableVehiclesDB;
+
+    }
+
+    // Print Vehicle list
+    public void printVehiclesAvailableToBuy(){
+        // Remove all the vehicles form the Garage from the VehiclesDB
+        System.out.println("You vehicles available Vehicles to buy:");
+        for (int i = 0; i < vehicleListDB.size(); i++) {
+            // Just print if is not in the garage
+            if(!garage.contains(vehicleListDB.get(i))){
+                System.out.println("    " + (i+1) + ". " + vehicleListDB.get(i));
+            }
+        }
     }
 
     // Generate random number (I don't know where to store)
-    private int generateRandomNumber(Integer min, Integer max){
-        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    private int generateRandomNumber(Integer max){
+        return ThreadLocalRandom.current().nextInt(0, max + 1);
     }
 
     @Override
